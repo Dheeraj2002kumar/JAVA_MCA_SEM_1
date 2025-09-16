af22c7fd384125cb38cfe03e1227111cc355eecf
Dheeraj Kumar <dk8549644@gmail.com>
2025-09-16 15:48:05 +0530

    add file

---
+++ session_4/program_4.java
@@ -0,0 +1,9 @@
+package session_4;
+
+// 4. take a user input and check entered value is 0 to 5 then print entered value otherwase enter valid number.
+
+public class program_4 {
+    public static void main(String[] arg){
+        
+    }
+}
